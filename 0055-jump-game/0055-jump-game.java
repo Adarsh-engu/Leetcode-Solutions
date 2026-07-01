c5cf9554ff51e81449ca4618932060db91230d51
Adarsh Engu <96770286+Adarsh-engu@users.noreply.github.com>
2026-07-01 18:30:59 +0530

    Time: 2 ms (88.71%), Space: 47.8 MB (62.32%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n=nums.length;
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            if(i>ans)
+            return false;
+            ans=Math.max(ans,i+nums[i]);
+        }
+        return true;
+    }
+}
